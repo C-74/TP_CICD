21c3f99b2969684043631f29eb0a9a03465f068e
Yanis BELLAHCENE <bellahceneyanis03@gmail.com>
2026-03-16 10:19:45 +0100

    Initialisation du projet

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello World depuis VueJS ! (Pipeline CI/CD)</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+h1 {
+  color: #42b983;
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
---
+++ frontend/tests/dummy.test.js
@@ -0,0 +1,4 @@
+// Un test très basique pour Jest
+test('un test très bidon pour valider le pipeline', () => {
+  expect(true).toBe(true);
+});
